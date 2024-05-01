43f6b3a2c70a5b1ccd41d8c792c41db6956f0077
Tim Vivian-Griffiths <tim.vivian.griffiths@causaly.com>
2024-05-01 09:21:50 +0100

    First commit of python LazyVim setup

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Setting the escape keymap
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode using jk" })
+
+-- Setting the comments
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcgv", { noremap = false })
---
+++ lua/plugins/indentscope.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.indentscope",
+  enabled = false,
+  opts = {
+    draw = {
+      animation = function()
+        return 0
+      end,
+    },
+  },
+}
